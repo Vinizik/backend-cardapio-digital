adc049e308916b57fee032d2f22ca2d25a8872e4
Vinicius Martins Cavalcanti <88601887+Vinizik@users.noreply.github.com>
2023-08-24 22:52:01 -0300

    Add files via upload

---
+++ Cardapio_Digital/src/main/java/com/example/cardapio/food/FoodRepository.java
@@ -0,0 +1,11 @@
+package com.example.cardapio.food;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+
+    Food findById(Integer id);
+
+}
